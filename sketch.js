6c13964771af53b2c354432b9d5c20451b0e3294
dnjs0228 <dnjsrud0228@naver.com>
2014-09-11 14:29:57 +0900

    수정

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 function setup() {
   createCanvas(640, 480);
-  //0: black, 255: white
-  //background(red, green, blue); 
-  background(255, 0, 0);
+ 
+  background(0, 0);
   stroke(255);
   noFill();
   line(10, 10, 90, 10);
