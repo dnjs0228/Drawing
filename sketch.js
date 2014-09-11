5c1c782af7efd2ae841bc10d84b107465b3116d7
dnjs0228 <dnjsrud0228@naver.com>
2014-09-11 13:38:05 +0900

    background sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+
+  //0: black, 255: white
+  //background(red, green, blue);
+  background(255, 0, 0);
   stroke(255);
   noFill();
   line(10, 10, 90, 10);
