655b760b50511879fc6145ada08874c6cfd4b7c3
dnjs0228 <dnjsrud0228@naver.com>
2014-09-11 13:39:29 +0900

    배경 sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 function setup() {
   createCanvas(640, 480);
-
-  //0: black, 255: white
-  //background(red, green, blue);
+//0: black, 255: white
+  //background(red, green, blue); 
   background(255, 0, 0);
   stroke(255);
   noFill();
